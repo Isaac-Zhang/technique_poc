bf038fd27541021a399e64ecfc39e37ad9f891ba
Isaac Zhang <zhangpan0614@126.com>
2019-08-20 08:56:56 +0800

    准备配置信息

---
+++ spring-transactional-poc/src/main/java/com/sxzhongf/spring/transcational/poc/TransactionalApplication.java
@@ -0,0 +1,18 @@
+package com.sxzhongf.spring.transcational.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * TransactionalApplication for 事务验证 poc 启动类
+ *
+ * @author <a href="mailto:magicianisaac@gmail.com">Isaac.Zhang | 若初</a>
+ * @since 2019/8/20
+ */
+@SpringBootApplication
+public class TransactionalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionalApplication.class, args);
+    }
+}
